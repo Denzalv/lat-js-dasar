2004a1b94bbadeabd660262414f6241b3ba19b03
Deni Alpiana <Denzalv01@gmail.com>
2021-10-19 11:27:32 +0700

    add cara mendeklarasikan variabel

---
+++ Mendeklarasikan Variabel/app.js
@@ -0,0 +1,11 @@
+// Contoh penulisan nama variabel yang benar
+
+let nama;
+let tempatTinggal;
+let _nomorHandphone;
+let $nomorAkunBank;
+// Contoh penulisan nama variabel yang salah
+
+// let makanan pembuka; //tidak boleh ada spasi
+// let @username; //tidak boleh diawali dengan @
+// let delete; //delete itu merupakan kata kunci di JavaScript
