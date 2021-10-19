f9946a5518cbbc251bbc757685809564e925bb45
Deni Alpiana <Denzalv01@gmail.com>
2021-10-19 11:39:11 +0700

    add cara menamppilkan ke consolelog

---
+++ menampilkan data di console/app.js
@@ -0,0 +1,10 @@
+let nama = "Budi";
+let umur = 20;
+let apakahSudahMenikah = false;
+let jumlahAnak;
+
+// Cobalah buka tab console di developer tools browser anda
+console.log(nama); // output: Budi
+console.log(umur); // output: 20
+console.log(apakahSudahMenikah); // output: false
+console.log(jumlahAnak); // output: undefined
